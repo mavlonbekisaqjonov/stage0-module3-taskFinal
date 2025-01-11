ad058ca40ed8b6a9e11dad4377132635f29b2d4c
Mavl0nbek <mavlonbekisaqjonov222@gmail.com>
2025-01-11 15:37:25 +0500

    done

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int out = 0;
+        for(int i=0; i<4; i++) {
+            out = out + number%10;
+            number = number / 10;
+        }
+        System.out.println(out);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int out = 0;
+        while (number != 0) { // Continue until the number becomes 0
+            out = out * 10 + number % 10; // Extract the last digit and build the reversed number
+            number = number / 10;         // Remove the last digit
+        }
+        System.out.println(out); // Print the reversed number
     }
 }
